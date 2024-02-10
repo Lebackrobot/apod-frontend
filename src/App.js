c75adbe22683a61123a6c3b70c2272ac297b3967
Lebackrobot <lebackman18@gmail.com>
2024-02-10 04:03:01 -0300

    ✨ feat: add MailerForm component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 
-import { Container, Col, Row } from 'react-bootstrap'
+import MailerForm from './components/Form/MailerForm';
+import nasaLogo from './images/nasa-logo.png'
+import { Container, Col, Row, Form, Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   return (
     <div>
       <Row>
-        <Col xs={4}>
+        <Col xs={4} className='d-flex align-items-center justify-content-center p-custom'>
           <Container>
+            <div className='image-container mb-5 pb-2'>
+              <img src={ nasaLogo } width='200' alt='Nasa logo'></img>
+            </div>
+            <h4>Astronomy Picture of The Day</h4>
+            <p className='subtext mb-4'>Receive daily in your email photos provided by NASA's API, APOD.</p>
+
+            <MailerForm></MailerForm>
 
           </Container>
         </Col>
--- src/components/Card.js
+++
@@ -1,8 +0,0 @@
-
-const Cards = () => {
-    return (
-        <div> teste </div>
-    )
-}
-
-export default Cards
