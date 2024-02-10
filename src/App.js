dd9e931d7193389020fb324924253edc159116a0
Lebackrobot <lebackman18@gmail.com>
2024-02-10 15:38:52 -0300

    ✨ feat: add contact me

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
-import MailerForm from './components/Form/MailerForm';
-import nasaLogo from './images/nasa-logo.png'
-import { Container, Col, Row, Form, Button } from 'react-bootstrap'
+import MailerForm from './components/Form/MailerForm'
+import { Container, Col, Row, Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import nasaLogo from './images/nasa-logo.png'
+import gmailIcon from './images/gmail-icon.png'
+import linkedinIcon from './images/linkedin-icon.jpeg'
+import githubIcon from './images/github-icon.png'
 
 const App = () => {
   return (
@@ -19,11 +23,33 @@ const App = () => {
 
             <MailerForm></MailerForm>
 
+            <Col>
+              <Row className='d-flex align-items-center justify-content-center'>
+                <span className='subtext mt-5 mb-5 d-flex align-items-center justify-content-center'> 
+                  &mdash;&nbsp;<strong> contact me </strong>&nbsp;&mdash;
+                </span>
+                
+                <Button href='https://github.com/Lebackrobot' variant='dark' className='mb-1'>
+                  <img src={githubIcon} width='20'></img>&nbsp;&nbsp;
+                  Github
+                  </Button>
+                  
+                <Button href='https://www.linkedin.com/in/pedro-fernandes-b72a8516b/' className='mb-1'>
+                  &nbsp;&nbsp;&nbsp;<img src={linkedinIcon} width='20'></img>&nbsp;
+                  Linkedin
+                </Button>
+                
+                <Button href='malto:dr.pedrofernades@gmail.com' variant='danger' className='mb-1'>
+                  <img src={gmailIcon} width='20'></img>&nbsp;
+                  Gmail</Button>
+              </Row>
+            </Col>
           </Container>
         </Col>
 
         <Col className="wallpaper-col" xs={8}></Col>
       </Row>
+
     </div>
   )
 }
