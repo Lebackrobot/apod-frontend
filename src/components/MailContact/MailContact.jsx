724637f5530f99b98a09827347613390190f2b7a
Lebackrobot <lebackman18@gmail.com>
2024-02-11 15:11:33 -0300

    🛠️ chore: responsive

--- src/components/MailContact/MailContact.jsx
+++ src/components/MailContact/MailContact.jsx
@@ -3,7 +3,7 @@ import styles from './MailContact.module.css'
 
 const MailContact = () => {
     return (
-        <span className={` ${styles.subtitle} subtext p-4 mt-4 mb-4 d-flex align-items-center justify-content-center`}>
+        <span className={` ${styles.subtitle} subtext p-3 mt-3 mb-3 d-flex align-items-center justify-content-center`}>
             &mdash;&nbsp;
             <strong> contact me </strong>
             &nbsp;&mdash;
