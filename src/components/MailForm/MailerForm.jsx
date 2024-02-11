2945ba3192f4535b0619fffce7ec510476336883
Lebackrobot <lebackman18@gmail.com>
2024-02-10 23:32:40 -0300

    🛠️ refactor: MailForm component

--- src/components/MailForm/MailerForm.jsx
+++ src/components/MailForm/MailerForm.jsx
@@ -1,12 +1,12 @@
 import { Form, Button } from 'react-bootstrap';
-import './MailerForm.css'
+import styles from './MailerForm.module.css'
 
 const MailerForm = () => {
     return (
             <Form>
                 <Form.Control style={{ backgroundColor:'#f9f9f9'}} className='mb-3' placeholder="Name"/>
                 <Form.Control style={{ backgroundColor: '#f9f9f9' }}  className='mb-3' type="email" placeholder="Email" />
-                <Button className='w-100 button-primary' type="submit">Send to me</Button>
+                <Button className={`w-100 button-primary ${styles.buttonPrimary}`} type="submit">Send to me</Button>
             </Form>
     )
 }
