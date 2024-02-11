c936107eade9960f1bc607f2adddbfeb83b040f2
Lebackrobot <lebackman18@gmail.com>
2024-02-10 22:12:13 -0300

    🛠️ reafactor: MailHeader component

---
+++ src/components/MailHeader/MailHeader.jsx
@@ -0,0 +1,16 @@
+import { Row } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import styles from './MailHeader.module.css'
+
+const MailerHeader = ({ children, subtitle, imageUrl }) => {
+    return (
+        <Row className='justify-content-center mb-5 pb-2'>
+            <img src={imageUrl} className={styles.imageLg} alt='Nasa icon image'/>
+            <h4>{ children }</h4>
+            <span className={styles.subtitle}>{ subtitle }</span>
+        </Row>
+    )
+}
+
+export default MailerHeader
